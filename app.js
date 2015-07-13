3ae630ff5dba50e75fb8f8b135ef6cddb8a5130c
cghil <cghilarducci@gmail.com>
2015-07-13 11:54:10 -0700

    testing gitignore

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ var nodemailer = require('nodemailer');
 
 var routes = require('./routes/index');
 var about = require('./routes/about');
-var contact = require('./routes/contact')
+var contact = require('./routes/contact');
 
 var app = express();
 
@@ -17,17 +17,16 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
 // uncomment after placing your favicon in /public
-//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+//app.use(favicon(__dirname + '/public/favicon.ico'));
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-// routes are defined above
 app.use('/', routes);
-app.use('/about', about);
-app.use('/contact', contact)
+app.use('/about',about);
+app.use('/contact',contact);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
